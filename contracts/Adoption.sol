9c748c5ea5e178022c6714b3e56cbaea2fb8801e
rebeccadimatteo <beccadimatteoo@gmail.com>
2023-02-24 10:13:58 +0100

    update html

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -1,20 +1,20 @@
 pragma solidity ^0.5.0;
 
 contract Adoption {
-address[16] public adopters;
+    address[16] public adopters;
 
 
- // Adopting a tree
- function adopt(uint treeId) public returns (uint) {
-   require(treeId >= 0 && treeId <= 15);
+    // Adopting a tree
+    function adopt(uint treeId) public returns (uint) {
+        require(treeId >= 0 && treeId <= 15);
 
-   adopters[treeId] = msg.sender;
+        adopters[treeId] = msg.sender;
 
-   return treeId;
- }
-// Retrieving the adopters
-function getAdopters() public view returns (address[16] memory) {
-  return adopters;
-}
+        return treeId;
+    }
+    // Retrieving the adopters
+    function getAdopters() public view returns (address[16] memory) {
+        return adopters;
+    }
 
 }
